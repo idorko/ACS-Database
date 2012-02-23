bc78074178f0acddb2444a289601e1bb87cd069d
Ian Dorko <idorko@gmail.com>
2012-02-23 16:22:09 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AcsDatabase::Application.config.secret_token = 'f9755f1f5ad53f4e0b2b1e28961bd699ff686c0c60f365e467e39e5bab4fff7b0fed999ffbe63e738a3e7719766c42d5bd316296bb9b3c8027b4296106ba8e7d'
